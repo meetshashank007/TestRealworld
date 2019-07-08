0f419dbacfd2fa71d6f40629c4adbb0ef97716e4
meetshashank007 <meet.shashank007@gmail.com>
2019-07-08 23:57:07 +0530

    base app commit

---
+++ config/keys.js
@@ -0,0 +1,6 @@
+module.exports = {
+
+    mongoURI : "mongodb+srv://master:oaGXALxcBFenaYxY@cluster0-1jtjt.mongodb.net/test?retryWrites=true",
+    secretOrKey : "secret"
+    //mongodb+srv://master:master@cluster0-1jtjt.mongodb.net/test?retryWrites=true
+}
